e6c6538b96ff6a3fed3474f362dec56b96f3c28e
Henrique Douglas <henriquedouglas2803@gmail.com>
2026-08-28 21:48:15 -0300

    adicioana estilo da tela login

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Route, Routes, useLocation } from "react-router";
+import Menu from "./components/Menu/Menu";
+import Login from "./pages/Login/Login";
+import Mesas from "./pages/Mesas/Mesas";
+import PedidosItems from "./pages/PedidosItems/PedidosItems";
+import Pedidos from "./pages/Pedidos/Pedidos";
+import Chefs from "./pages/Chefs/Chefs";
+
+// const telasNaoMenu = ["/", "/fale-conosco"];
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <>
+      {/* !telasNaoMenu.includes(location.pathname) && <Menu /> */}
+      {location.pathname !== "/" && <Menu />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/mesas" element={<Mesas />} />
+        <Route path="/pedido-items" element={<PedidosItems />} />
+        <Route path="/pedidos" element={<Pedidos />} />
+        <Route path="/chefes" element={<Chefs />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
 
-import Login from "./pages/Login/Login";
-import Mesas from "./pages/Mesas/Mesas";
-import PedidosItems from "./pages/PedidosItems/PedidosItems";
-import Pedidos from "./pages/Pedidos/Pedidos";
+import App from "./App";
 
 import "./styles/reset.css";
 import "./index.module.css";
-import Chefs from "./pages/Chefs/Chefs";
-import Menu from "./components/Menu/Menu";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/mesas" element={<Mesas />} />
-        <Route path="/pedido-items" element={<PedidosItems />} />
-        <Route path="/pedidos" element={<Pedidos />} />
-        <Route path="/chefes" element={<Chefs />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
